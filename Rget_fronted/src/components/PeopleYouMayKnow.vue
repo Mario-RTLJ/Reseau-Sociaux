c11d29368904d588dfd82ef808c3be1f9c9d9e52
Mario-RTLJ <marioratolojanahary58@gmail.com>
2023-10-23 17:48:54 +0300

    first commit

---
+++ Rget_fronted/src/components/PeopleYouMayKnow.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="p-4 bg-white border border-gray-200 rounded-lg">
+        <h3 class="mb-6 text-xl">People you may know</h3>
+
+        <div class="space-y-4">
+            <div 
+                class="flex items-center justify-between"
+                v-for="user in users"
+                v-bind:key="user.id"
+            >
+                <div class="flex items-center space-x-2">
+                    <img :src="user.get_avatar" class="w-[40px] rounded-full">
+                    
+                    <p class="text-xs"><strong>{{ users.name }}</strong></p>
+                </div>
+
+                <RouterLink :to="{name: 'profile', params: {id: user.id}}" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Show</RouterLink>>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios"
+
+export default (await import('vue')).defineComponent({
+    data() {
+        return {
+            users: []
+        }
+    },
+
+    mounted () {
+        this.getFriendSuggestions()
+    },
+
+    methods: {
+        getFriendSuggestions () {
+            axios
+                .get('/api/friends/suggested/')
+                .then(Response => {
+                    console.log(Response.data)
+                    this.users = Response.data
+                })
+                .catch(error => {
+                    console.log('error', error)
+                })
+        }
+    }
+})
+</script>
---
+++ Rget_fronted/src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <h1 class="text-center font-bold">Welcome</h1>
+  </main>
+</template>
